5663ca9124e10fc0e2bdb6440a87c9d5daa2ab31
kimcocru <kimcocru@gmail.com>
2018-01-19 10:30:54 -0600

    Todo finished

--- index.js
+++ index.js
@@ -1,59 +1,91 @@
-var yo = require('yo-yo')
-
-const uuidv1 = require('uuid/v1');
+var yo = require("yo-yo")
+const uuidv1 = require("uuid/v1")
 
 var numbers = [] // start empty
-var el = list(numbers, update)
-var state = []
-var counter = 0;
+var el = list(numbers, update, deleteTodo)
 
-
-function list (items, onclick) {
-  return yo`<div>
-  <h1> Things To Do </>
-    <div class="ctas">
-      <input type="text" id="todoVal">
-      <button onclick=${onclick}>Add Todo</button>
-    </div>
-    <ul id="todoList">
-      ${items.map(function (item) {
-        return yo`<li>${item.value}<button onclick=${removeTodo}>Delete</button></li>`
-      })}
-    </ul>
-    <h1> Done:</h1>
-
-  </div>`
+function list (items, onclick, deleteTodo){
+ return yo`<main role="main">
+   <h1>Things ToDo:</h1>
+   <div class=“ctas”>
+     <input type=“text” id="todoVal">
+     <button id="addTodoButt" class="button-secondary pure-button" onclick=${onclick}>Add To-do</button>
+   </div>
+   <ul id="todoList">
+     ${items.map(function (item) {
+       if(item.status === "pending"){
+         return yo`<li id=${item.id}>
+         ${item.value}
+         <input type="checkbox" onclick=${deleteTodo} class="btn">
+         </li>`
+       }
+     })}
+   </ul>
+   <h1>Done:</h1>
+   <ul id=“doneList”>
+     ${items.map(function (item) {
+       if(item.status === "done"){
+         return yo`<li id=${item.id}>
+         ${item.value}
+         <button onclick=${deleteTodo} class=“btn”>Pending</button>
+         </li>`
+       }
+     })}
+   </ul>
+ </main>`
 }
 
-function removeTodo(ev) {
-  var id = ev.target.parentNode.getAttribute('id')
-  state = state.filter(function(el, i) {
-    return id !== el.id;
-  });
 
-  ev.target.parentNode.remove();
-  console.table(state);
-}
 function update () {
-  // add a new random number to our list
-  var todo={}
-  todo.id = uuidv1();
-  todo.value = document.getElementById("todoVal").value
-  todo.status = 'pending'
-  state = [todo.id, todo.value, todo.status]
-  numbers.push(todo)
+ // add a new random number to our list
+ var todo = {};
+   todo.id = uuidv1();
+   todo.value = document.getElementById("todoVal").value
+   todo.status = "pending";
 
-  console.table(state);
+ numbers.push(todo)
 
-  // construct a new list and efficiently diff+morph it into the one in the DOM
-  var newList = list(numbers, update)
-  yo.update(el, newList)
-  yo.removeTodo()
-  counter+1;
-  
+ // construct a new list and efficiently diff+morph it into the one in the DOM
+ var newList = list(numbers, update, deleteTodo)
+ yo.update(el, newList)
+ document.getElementById("todoVal").value = ""
 }
 
+
+
+function deleteDone(ev){
+   var id = ev.target.parentNode.getAttribute("id");
+   state = state.filter(function(el, i) {
+      return id !== el.id;
+    });
+
+    ev.target.parentNode.remove();
+    var newList = list(numbers, update, deleteDone)
+    yo.update(el, newList)
+
+
+}
+function deleteTodo(ev){
+ var id = ev.target.parentNode.getAttribute("id");
+
+
+ numbers = numbers.filter(function(el){
+   if(el.id === id){
+     if(el.status === "pending"){
+       el.status = "done"
+
+     }
+     else{
+       el.status = "pending"
+     }
+   }
+
+
+   return true;
+ })
+
+ var newList = list(numbers, update, deleteTodo)
+ yo.update(el, newList)
+}
 
 document.body.appendChild(el)
-
-console.log("HOLA kiM")
