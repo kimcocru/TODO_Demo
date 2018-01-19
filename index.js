c97603e5a4a70e4e0d9bb291a4921d8dd35d42b8
kimcocru <kimcocru@gmail.com>
2018-01-19 13:47:36 -0600

    agregar modulo EventEmitter

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var yo = require("yo-yo")
 const uuidv1 = require("uuid/v1")
+import EventEmitter from 'events'
 
 var numbers = [] // start empty
 var el = list(numbers, update, deleteTodo)
@@ -14,10 +15,7 @@ function list (items, onclick, deleteTodo){
    <ul id="todoList">
      ${items.map(function (item) {
        if(item.status === "pending"){
-         return yo`<li id=${item.id}>
-         ${item.value}
-         <input type="checkbox" onclick=${deleteTodo} class="btn">
-         </li>`
+          return CreateLi(item);
        }
      })}
    </ul>
@@ -25,16 +23,27 @@ function list (items, onclick, deleteTodo){
    <ul id=“doneList”>
      ${items.map(function (item) {
        if(item.status === "done"){
-         return yo`<li id=${item.id}>
-         ${item.value}
-         <button onclick=${deleteTodo} class=“btn”>Pending</button>
-         </li>`
+         return CreateLi2(item);
        }
      })}
    </ul>
  </main>`
 }
 
+
+function CreateLi(item){
+  return yo`<li id=${item.id}>
+  ${item.value}
+  <input type="checkbox" onclick=${deleteTodo} class="btn">
+  </li>`
+}
+
+function CreateLi2(item){
+  return yo`<li id=${item.id}>
+  ${item.value}
+  <input type="button" class="button-secondary pure-button" onclick=${deleteTodo} class="btn">
+  </li>`
+}
 
 function update () {
  // add a new random number to our list
@@ -51,36 +60,17 @@ function update () {
  document.getElementById("todoVal").value = ""
 }
 
-
-
-function deleteDone(ev){
-   var id = ev.target.parentNode.getAttribute("id");
-   state = state.filter(function(el, i) {
-      return id !== el.id;
-    });
-
-    ev.target.parentNode.remove();
-    var newList = list(numbers, update, deleteDone)
-    yo.update(el, newList)
-
-
-}
 function deleteTodo(ev){
  var id = ev.target.parentNode.getAttribute("id");
-
-
  numbers = numbers.filter(function(el){
    if(el.id === id){
      if(el.status === "pending"){
        el.status = "done"
-
      }
      else{
        el.status = "pending"
      }
    }
-
-
    return true;
  })
 
